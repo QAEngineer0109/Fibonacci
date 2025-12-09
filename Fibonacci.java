7bd9797949f4407a0927af45ef38d1d6290b8958
QAEngineer0109 <skippy@sc.rr.com>
2025-12-09 10:22:32 -0500

    Add files via upload
    Uploaded Fibonacci generator

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package day6;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+	System.out.println("Enter the number of terms in a Fibonacci series:");
+	int terms= scanner.nextInt();
+	int a=0, b=1;
+	System.out.println("Fibonacci Sequence:");
+	
+	for (int i=1; i<=terms;i=i+1)
+	{System.out.println(a+ "");
+	int next=a+b;
+	a=b;
+	b=next;
+	}
+
+	}
+	
+}
